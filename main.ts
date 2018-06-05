e1296de881869e91b78feb85de7fa780792f5ee9
Ana Lima <analima@tokenlab.com.br>
2018-06-05 13:39:39 -0300

    First commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+const imagemin = require('imagemin');
+const imageminWebp = require('imagemin-webp');
+import { ArgumentParser } from 'argparse';
+import * as shell from 'shelljs';
+
+const parser = new ArgumentParser({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'Minify images'
+});
+parser.addArgument(
+    ['-d', '--dir'],
+    {
+        help: 'Directory to read big images, write small images and erase big images',
+        required: true,
+    }
+);
+
+const args = parser.parseArgs();
+
+shell.cd(__dirname);
+shell.cd(args.dir);
+const pathDir = shell.pwd().stdout as string;
+ 
+imagemin([pathDir+'/*.{jpg,png}'], pathDir, {
+    use: [
+        imageminWebp({lossless: true})
+    ]
+}).then(() => {
+    console.log('Images optimized');
+    shell.rm('-r', pathDir+'/*.{jpg, png}');
+});
